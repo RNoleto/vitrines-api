c7090eb789b3619537ecb5017534e83b937499c4
Ramon Noleto <devnoleto@gmail.com>
2025-04-18 12:12:51 -0300

    Habilitando as rotas api para consumo no frontend.

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
+    return response()->json(['message' => 'pong from API']);
 });
