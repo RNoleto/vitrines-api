97fece92dc19391b3b814967647061355153b9a8
Ramon Noleto <devnoleto@gmail.com>
2025-04-23 10:38:23 -0300

    Test ContactStoresController.php

--- app/Http/Controllers/ContactStoresController.php
+++ app/Http/Controllers/ContactStoresController.php
@@ -12,14 +12,10 @@ class ContactStoresController extends Controller
     public function index()
     {
         $user = auth()->user();
-
-        // Pegando a loja do usuário
         $store = $user->store;
 
-        // Se a loja existir, buscar os contatos da loja
         if ($store) {
-            $contacts = ContactStore::all();
-
+            $contacts = ContactStore::where('store_id', $store->id)->get();
             return response()->json($contacts);
         }
 
@@ -31,27 +27,26 @@ class ContactStoresController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'user_id' => 'required|exists:users,id',
             'store_id' => 'required|exists:stores,id',
             'name' => 'required|string|max:255',
             'whatsapp' => 'required|string|max:255',
             'photo' => 'nullable|mimes:jpg,jpeg,png,svg,webp',
         ]);
-
+    
         $path = null;
-
-        if($request->hasFile('photo')){
+    
+        if ($request->hasFile('photo')) {
             $path = $request->file('photo')->store('contacts_photo', 'public');
         }
-
+    
         $contact = ContactStore::create([
-            'user_id' => $request->user_id,
+            'user_id' => auth()->id(), // aqui
             'store_id' => $request->store_id,
             'name' => $request->name,
             'whatsapp' => $request->whatsapp,
             'photo' => $path,
         ]);
-
+    
         return response()->json($contact, 201);
     }
 }
