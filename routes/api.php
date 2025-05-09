8e5ae1e08ea9f11234c559ac8d067c12e76be8eb
Ramon Noleto <devnoleto@gmail.com>
2025-05-09 13:37:13 -0300

    Teste de rotas Administrativas mais limpa

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\StoreController;
 use App\Http\Middleware\FirebaseAuthenticate;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
+use App\Http\Middleware\FirebaseAuth;
 
 Route::get('/users', [UserController::class, 'users']);
 
@@ -53,12 +54,23 @@ Route::get('/public/stores/{store}/contacts', [ContactController::class, 'public
 Route::get('/usuarios/firebase/{firebase_uid}', [UserController::class, 'buscarPorFirebase']);
 
 
-Route::middleware([
-    FirebaseAuthenticate::class,
-    \App\Http\Middleware\CheckRole::class.':admin'
-])->group(function(){
-    Route::get('/admin/dashboard', function() {
-        return response()->json(['message' => 'Bem-vindo, administrador']);
+// Route::middleware([
+//     FirebaseAuthenticate::class,
+//     \App\Http\Middleware\CheckRole::class.':admin'
+// ])->group(function(){
+//     Route::get('/admin/dashboard', function() {
+//         return response()->json(['message' => 'Bem-vindo, administrador']);
+//     });
+//     Route::get('/admin/contacts', [ContactController::class, 'adminIndex']);
+// });
+
+// Rotas Administrativas (Dashboard Admin)
+Route::middleware(FirebaseAuthenticate::class)->group(function() {
+    Route::middleware('role:admin')->group(function() {
+        Route::get('/admin/dashboard', function() {
+            return response()->json(['message' => 'Bem-vindo, administrador']);
+        });
+
+        Route::get('/admin/contacts', [ContactController::class, 'adminIndex']);
     });
-    Route::get('/admin/contacts', [ContactController::class, 'adminIndex']);
 });
